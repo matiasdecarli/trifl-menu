86d97706f04aef04ad7c250a83681e2eb742d927
Matias De Carli <matiasdecarli@gmail.com>
2015-04-24 01:49:56 -0300

    quick refactor

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# trifl-scrapper
+# trifl-menu
 a way to bring the trifl menu to slack
 
 # instalation
-1 - ```npm install``` to install de dependencies  
-2- open the file ```scrapper.js``` and complete with your slack info  [Slack API](https://api.slack.com/)  
-3- run ```npm start``` or ```node scrapper.js```
+1 - ```npm install``` to install the dependencies  
+2- open the file ```package.json``` and complete with your slack info  [Slack API](https://api.slack.com/), and preferences  
+3- run ```npm start``` or ```node menu.js``` to post the menu to Slack
--- menu.js
+++ menu.js
@@ -1,7 +1,8 @@
 var request = require("request");
+var pjson = require('./package.json');
 
 request.post({
-    url: 'https://api.parse.com/1/classes/IterationModel',
+    url: pjson.trifl_url,
     form: '{"where":{"franchise":{"__type":"Pointer","className":"Franchise","objectId":"PCNSFmlKdN"},"status":"active"},"include":"options,chef,franchise","order":"-createdAt","limit":1,"_method":"GET","_ApplicationId":"OiyqYtObwNjjFvu4MSwptTCNiT2IVbAqGfFUQnIB","_JavaScriptKey":"R71nTJgZN2BwUMA6mnTerqrFSl3lWG0ZfKEq0EIo","_ClientVersion":"js1.2.18","_InstallationId":"c84bba51-6be8-00f2-b1ac-602afeed724a"}'
 }, function(err, response) {
     if (response.statusCode === 200) {
@@ -16,14 +17,15 @@ function parseMenu(body) {
 
     var options = 1;
 
-    var special_item = '';
-    var beverage = '';
+    var food = '';
+    var special_item = 'Extras: ';
+    var beverage = 'Bebidas: ';
 
     for (var i = 0; i < menu.options.length; i++) {
         if (!menu.options[i].sold_out || menu.options[i].sold_out === false) {
             switch (menu.options[i].type) {
                 case 'food':
-                    postToSlack((options++) + '. ' + menu.options[i].name + ': ' + menu.options[i].description + ' ' + menu.options[i].picture.url);
+                    food = food + (options++) + '. ' + menu.options[i].name + ': ' + menu.options[i].description + ' ' + menu.options[i].picture.url + '\n';
                     break;
                 case 'beverage':
                     beverage = beverage + menu.options[i].name + '(' + menu.options[i].description + '), ';
@@ -33,32 +35,32 @@ function parseMenu(body) {
                     break
             }
         }
-    };
+    };    
 
-    postToSlack('Extras: ' + special_item.substring(0, special_item.length - 2) + ', Bebidas: ' + beverage.substring(0, beverage.length - 2));
-
-    var time_slots = '';
+    var time_slots = 'Horarios Disponibles: ';
     for (var i = 0; i < menu.time_slots.length; i++) {
         if (menu.time_slots[i].sold_out !== true) {
             time_slots = time_slots + menu.time_slots[i].label + ', ';
         }
     };
-    postToSlack('Horarios Disponibles: ' + time_slots.substring(0, time_slots.length - 2));
+
+    var content = ((pjson.send_food) ? food : '') +
+        ((pjson.send_beverages) ? beverage.substring(0, beverage.length - 2) + '\n' : '') +
+        ((pjson.send_special_item) ? special_item.substring(0, special_item.length - 2) + '\n' : '') +
+        ((pjson.send_timeframes) ? time_slots.substring(0, time_slots.length - 2) + '\n' : '');
+    
+    postToSlack(content);
 };
 
 function postToSlack(payload) {
-    var slack_base_url = 'https://slack.com/api/chat.postMessage';
-    var slack_token = //YOUR SLACK TOKEN;
-    var slack_channel = '#feeds';
-
     request.post({
-        url: slack_base_url,
+        url: pjson.slack_base_url,
         form: {
-            token: slack_token,
-            channel: slack_channel,
+            token: pjson.slack_token,
+            channel: pjson.slack_channel,
             text: payload,
-            username: 'Trifl',
-            icon_url: 'https://pbs.twimg.com/profile_images/507563941078114304/0Ub4z1AG_400x400.png',
+            username: pjson.bot_username,
+            icon_url: pjson.bot_avatar,
             pretty: 1
         }
     }, function(err, response) {
