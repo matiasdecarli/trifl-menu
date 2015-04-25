26dc111200cd3e2458d14f96b1c83ac7f536c2d6
Matias De Carli <matiasdecarli@gmail.com>
2015-04-25 16:13:01 -0300

    get menu is now a route

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # trifl-menu
-a way to bring the trifl menu to slack
+a way to bring the trifl menu to slack, and get the orders from the dudes & dudettes on your team
 
 # instalation
-1 - ```npm install``` to install the dependencies  
+1- ```npm install``` to install the dependencies  
 2- open the file ```package.json``` and complete with your slack info  [Slack API](https://api.slack.com/), and preferences  
-3- run ```npm start``` or ```node menu.js``` to post the menu to Slack
+3- run ```npm start``` or ```node menu.js``` to start the app.
+
+# post the menu
+just put a webook like '/menu'. if theres a number after the '/menu' it will start a voting with the selected time. /menu{time in minutes}
+
+# voting feature
+to use the voting feature you must turn on ```voting_enabled```, create an outgoing webook in [Slack API - Webhooks](https://api.slack.com/outgoing-webhooks) and point the URL to your URL/voting. Also you need to configure the token, and the trigger
+
+if you want a hook to finish voting, same story than before with url pointing to URL/voting/end
+
+# package.json vars
+```slack_base_url``` Slack base url. I wouldnt touch this one    
+```slack_token``` The Slack Token (for the user posting data)    
+```slack_webook_token_vote``` The token of the webook trigger that count votes    
+```slack_webook_token_end``` The token of the webook trigger that end voting    
+```slack_channel``` The channel in which Slak will post the menu    
+```bot_username``` The name of the bot that will be posting the menu    
+```bot_avatar``` The avatar url of the bot that will be posting the menu    
+```trifl_url``` The url of Trifl. Again, dont touch this unless you know what you're doing     
+```send_food``` This enables the Food part of the menu    
+```send_beverages``` This enables the Beverage part of the menu    
+```send_special_item``` This enables the Special Item part of the menu    
+```send_timeframes``` This enables the Time window part of the menu    
--- menu.js
+++ menu.js
@@ -1,18 +1,99 @@
 var request = require("request");
 var pjson = require('./package.json');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
+var ended = false;
+var votes = [];
 
-request.post({
-    url: pjson.trifl_url,
-    form: '{"where":{"franchise":{"__type":"Pointer","className":"Franchise","objectId":"PCNSFmlKdN"},"status":"active"},"include":"options,chef,franchise","order":"-createdAt","limit":1,"_method":"GET","_ApplicationId":"OiyqYtObwNjjFvu4MSwptTCNiT2IVbAqGfFUQnIB","_JavaScriptKey":"R71nTJgZN2BwUMA6mnTerqrFSl3lWG0ZfKEq0EIo","_ClientVersion":"js1.2.18","_InstallationId":"c84bba51-6be8-00f2-b1ac-602afeed724a"}'
-}, function(err, response) {
-    if (response.statusCode === 200) {
-        parseMenu(JSON.parse(response.body));
-    } else {
-        console.log(err);
-    }
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/menu/', function(req,res){
+    if (!req.body || !req.body.token || !pjson.slack_webook_token_menu || (req.body.token !== pjson.slack_webook_token_menu))
+        return res.sendStatus(401);
+
+    votes = [];
+
+    request.post({
+        url: pjson.trifl_url,
+        form: '{"where":{"franchise":{"__type":"Pointer","className":"Franchise","objectId":"PCNSFmlKdN"},"status":"active"},"include":"options,chef,franchise","order":"-createdAt","limit":1,"_method":"GET","_ApplicationId":"OiyqYtObwNjjFvu4MSwptTCNiT2IVbAqGfFUQnIB","_JavaScriptKey":"R71nTJgZN2BwUMA6mnTerqrFSl3lWG0ZfKEq0EIo","_ClientVersion":"js1.2.18","_InstallationId":"c84bba51-6be8-00f2-b1ac-602afeed724a"}'
+    }, function(err, response) {
+        if (response.statusCode === 200)
+            parseMenu(JSON.parse(response.body), function(err) {
+                if (err) console.log(err);    
+
+                var command = req.body.text;
+                var time = command.split('/menu');                
+                
+                if (time[1]) {
+                    ended = false;
+
+                    var timeLimit = addMinutes(new Date(),parseInt(time[1]));
+
+                    postToSlack("Voting enabled: To vote add '+' before your order. Time limit: " +
+                        (timeLimit.getHours()-3) + ':' + timeLimit.getMinutes() , function(){
+                        setTimeout(function() {
+                            if (ended) return;
+
+                            endVoting(function() {
+                                return;
+                            });
+                        }, parseInt(time[1])*60000);
+                    })                    
+                }                
+
+                return res.sendStatus(200);
+            });
+        else
+            console.log(err);
+    });
 });
 
-function parseMenu(body) {
+app.post('/voting/', function(req, res) {
+    if (!req.body || !req.body.token || !pjson.slack_webook_token_vote || (req.body.token !== pjson.slack_webook_token_vote))
+        return res.sendStatus(401);
+
+    votes[req.body.user_name] = req.body.text;
+
+    return res.sendStatus(200);
+});
+
+app.post('/voting/end', function(req, res) {
+    if (ended || !req.body || !req.body.token || !pjson.slack_webook_token_end || (req.body.token !== pjson.slack_webook_token_end))
+        return res.sendStatus(401);
+
+    ended = true;
+
+    endVoting(function(){
+        return;
+    });
+});
+
+var server = app.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port;    
+});
+
+function addMinutes(date, mins) {
+    return new Date(date.getTime() + mins*60000);
+}
+
+function endVoting(next) {
+    var results = '';
+
+    for (var item in votes) {
+        results = results + item + ' : ' + votes[item] + '\n';
+    }
+
+    return postToSlack('Time\'s up! :clock1130: Lets order! ' + '\n' + results, function() {
+        return next();
+    });
+}
+
+function parseMenu(body, next) {
     var menu = body.results[0];
 
     var options = 1;
@@ -22,7 +103,7 @@ function parseMenu(body) {
     var beverage = 'Bebidas: ';
 
     for (var i = 0; i < menu.options.length; i++) {
-        if (!menu.options[i].sold_out || menu.options[i].sold_out === false) {
+        //if (!menu.options[i].sold_out || menu.options[i].sold_out === false) {
             switch (menu.options[i].type) {
                 case 'food':
                     food = food + (options++) + '. ' + menu.options[i].name + ': ' + menu.options[i].description + ' ' + menu.options[i].picture.url + '\n';
@@ -34,8 +115,8 @@ function parseMenu(body) {
                     special_item = special_item + menu.options[i].name + '(' + menu.options[i].description + '), ';
                     break
             }
-        }
-    };    
+        //}
+    };
 
     var time_slots = 'Horarios Disponibles: ';
     for (var i = 0; i < menu.time_slots.length; i++) {
@@ -48,12 +129,12 @@ function parseMenu(body) {
         ((pjson.send_beverages) ? beverage.substring(0, beverage.length - 2) + '\n' : '') +
         ((pjson.send_special_item) ? special_item.substring(0, special_item.length - 2) + '\n' : '') +
         ((pjson.send_timeframes) ? time_slots.substring(0, time_slots.length - 2) + '\n' : '');
-    
-    postToSlack(content);
+
+    return postToSlack(content, next);
 };
 
-function postToSlack(payload) {
-    request.post({
+function postToSlack(payload, next) {
+    return request.post({
         url: pjson.slack_base_url,
         form: {
             token: pjson.slack_token,
@@ -64,6 +145,8 @@ function postToSlack(payload) {
             pretty: 1
         }
     }, function(err, response) {
-        if (err) console.log(err);
+        if (err) return next(err);
+
+        return next();
     });
 };
